784b02546d65146916d240de49ba17dbc502647b
jstokesVMASC <jestokes@odu.edu>
2021-09-01 11:21:29 -0400

    First run updates & Rotator framerate fix

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
+    public float speed = 5;
 
-	public float speed = 5;
+    private void Start()
+    {
+        speed *= 60;
+    }
 
-	private float currentAngle = 0;
-	
-	// Update is called once per frame
-	void Update () {
-		currentAngle += speed;
-		if (currentAngle > 360)
-			currentAngle -= 360;
-		
-		transform.localRotation = Quaternion.Euler (0,0,currentAngle);
-	}
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
 }
